0489db4bf0ac01205a34e9fd1bfcd99e08f5040d
vs-ramya <sramya8051@gmail.com>
2024-02-19 23:24:41 +0530

    Fetch datas from swapi api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const[movies,setmovies]=useState([]);
+  const[isLoading,setisLoading]=useState(false);
+ async function fetchHandler() {
+  // we didn't mention any crud method here in default it is GET
+   const response=await fetch('https://swapi.dev/api/films/');
+   //Simplest way  to convert into json using fetch method inorder to arrange it in json (readable format)
+   const data=await response.json();
+  //map over the data to view particular datas
+      const transformedMovies=data.results.map(movieData => {
+        return {
+          id:movieData.episode_id,
+          title:movieData.title,
+          openingText:movieData.opening_crawl,
+          releaseDate:movieData.release_date
+        }
+      })
+      setmovies(transformedMovies);
+      setisLoading(false);
+  
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading  && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===0 && <h2>CLICK THE BUTTON TO FETCH MOVIES LIST</h2>}
+       
+        {isLoading && <h2>LOADING ....</h2>}
+      </section>
+    </React.Fragment>
   );
 }
 
